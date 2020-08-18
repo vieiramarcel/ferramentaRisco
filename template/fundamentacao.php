62a71644265279448c20d3647fda0464509fcbc4
marcel <marcel.fv70@gmail.com>
2020-08-17 22:51:44 -0300

    first commit

---
+++ template/fundamentacao.php
@@ -0,0 +1,60 @@
+<?php
+  include("cabecalho.php")
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>RM-AGILE</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet"  href="fundamentacao.css" />
+    <link rel="shortcut icon" href="../Imagens/favicon.png" type="image/x-png"/>
+    <link rel="stylesheet"  href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" >
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script> 
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  
+</head>
+
+<body>
+    <!--
+<iframe src="https://h5p.org/h5p/embed/439596" width="1090" height="2556" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>
+-->
+    <iframe src="http://localhost/gestaoAgilrisco/wp-admin/admin-ajax.php?action=h5p_embed&id=1" width="1090" height="2556" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="http://localhost/gestaoAgilrisco/wp-content/plugins/h5p/h5p-php-library/js/h5p-resizer.js" charset="UTF-8"></script>
+<!--
+    <nav class = "navegar">
+        <div class="contact-form">
+        <div class = "item">
+            <input type="checkbox" id="check1">
+            <label for="check1">Introdução</label>
+            <ul>
+                <li><a href="">Tutorial</a></li>
+                <li><a href="">Apresentação</a></li>
+            </ul>
+            </div>
+
+            <div class = "item">
+            <input type="checkbox" id="check2">
+            <label for="check2">Conceitos chaves</label>
+            <ul>
+                <li><a href="">Metodologias ágeis</a></li>
+                <li><a href="">Gerência de projetos</a></li>
+                <li><a href="">Riscos em projetos de software</a></li>
+            </ul>
+
+            <div class = "item">
+            <input type="checkbox" id="check3">
+            <label for="check3">Gerenciamento ágil de riscos</label>
+            <ul>
+                <li><a href="">Papéis</a></li>
+                <li><a href="">Cerimônias</a></li>
+                <li><a href="">Atividades</a></li>
+                <li><a href="">Ferramentas e técnicas</a></li>
+            </ul>
+</div>
+</div>
+ 
+</nav>  -->   
+</body>  
+</html>
